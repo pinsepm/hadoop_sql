e46f2501a4c9a3b04738ab34bad80d0436b748ab
pinsepm <93735460+pinsepm@users.noreply.github.com>
2021-11-04 15:37:14 -0700

    Create AdvertiserTopParents.sql

---
+++ AdvertiserTopParents.sql
@@ -0,0 +1,40 @@
+SELECT
+  adaptive_month,
+  adaptive_level,
+  SUM(billable_rev_sales_geo_fixed) AS billable_rev_sales_geo_fixed,
+  rank_concentration
+FROM
+
+(
+SELECT
+  adaptive_month,
+  adaptive_level,
+  billable_rev_sales_geo_fixed,
+  CASE WHEN revenue_rank <= 10 THEN 'Top 10 Advertisers'
+  WHEN revenue_rank <= 100 THEN 'Advertisers #11 - 100'
+  WHEN revenue_rank <= 500 THEN 'Advertisers #101 - 500'
+  ELSE 'Advertisers #501+' END AS rank_concentration
+FROM
+  (
+    --Global
+    SELECT
+      date(concat(CAST(year AS varchar),'-',CAST(quarter * 3 AS varchar),'-01')) AS adaptive_month,
+      'Total Pinterest' AS adaptive_level,
+      advertiser_id,
+      SUM(revenue_fixed_fx_by_year) AS billable_rev_sales_geo_fixed,
+      rank () over (
+        partition BY year,
+        quarter
+        ORDER BY
+          SUM(revenue_fixed_fx_by_year) DESC
+      ) revenue_rank
+    FROM
+      salesfinance.northstar_adstats_advertiser
+    GROUP BY
+      year,
+      quarter,
+      advertiser_id
+  )
+)
+GROUP BY adaptive_month, adaptive_level, rank_concentration
+ORDER BY adaptive_month, rank_concentration
