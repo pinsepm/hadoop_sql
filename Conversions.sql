8c88ac009f7a63c334a42347720d20cec6978438
pinsepm <93735460+pinsepm@users.noreply.github.com>
2021-11-04 15:40:37 -0700

    Create Conversions.sql

---
+++ Conversions.sql
@@ -0,0 +1,26 @@
+SELECT
+  date(
+    concat(
+      CAST(year AS varchar),
+      '-',
+      CAST(month AS varchar),
+      '-01'
+    )
+  ) AS adaptive_month,
+  'Total Pinterest' AS adaptive_level,
+  CASE
+    WHEN user_country = 'US' THEN 'c_US_Conversions'
+    ELSE 'c_INTL_Conversions'
+  END AS account,
+  sum(conversions_checkouts_771) AS conversion_count
+FROM
+  Finops.conversion_metrics
+WHERE
+  year >= 2019
+GROUP BY
+  year,
+  month,
+  CASE
+    WHEN user_country = 'US' THEN 'c_US_Conversions'
+    ELSE 'c_INTL_Conversions'
+  END
