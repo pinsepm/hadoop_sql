6aae5e46b0736f6df8245a3ee75b1e2e00f1fa11
victorhongpinterest <92404950+victorhongpinterest@users.noreply.github.com>
2022-03-03 09:24:01 -0800

    Update Sessions.sql
    Changing to add countries

--- Sessions.sql
+++ Sessions.sql
@@ -10,7 +10,7 @@ SELECT
     )
   ) AS adaptive_month,
   'Total Pinterest' AS Level,
-  CASE WHEN country = 'US' THEN 'c_US_Sessions' ELSE 'c_INTL_Sessions' END AS Account,
+  country,
   COUNT(unified_session_id) AS amount
 FROM
   data.attributed_sessions_base
@@ -18,7 +18,7 @@ WHERE
   is_dau = TRUE
   AND CAST(substring(dt, 1, 4) AS int) >= 2019
 GROUP BY
-  CASE WHEN country = 'US' THEN 'c_US_Sessions' ELSE 'c_INTL_Sessions' END,
+ country,
   date(
     concat(
       substring(dt, 1, 4),
