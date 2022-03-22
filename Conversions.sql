4daaa813909262c6326666c352fa4bfda11aa296
victorhongpinterest <92404950+victorhongpinterest@users.noreply.github.com>
2022-03-22 14:34:57 -0700

    Update Conversions.sql
    Re-edited to pull in countries

--- Conversions.sql
+++ Conversions.sql
@@ -1,30 +1,8 @@
--- For JSON Conversion
--- {"sql":"
-SELECT
-  date(
-    concat(
-      CAST(year AS varchar),
-      '-',
-      CAST(month AS varchar),
-      '-01'
-    )
-  ) AS adaptive_month,
-  'Total Pinterest' AS Level,
-  CASE
-    WHEN user_country = 'US' THEN 'c_US_Conversions'
-    ELSE 'c_INTL_Conversions'
-  END AS Account,
-  sum(conversions_checkouts_771) AS amount
-FROM
-  Finops.conversion_metrics
-WHERE
-  year >= 2019
-GROUP BY
-  year,
-  month,
-  CASE
-    WHEN user_country = 'US' THEN 'c_US_Conversions'
-    ELSE 'c_INTL_Conversions'
-  END
--- JSON Conversion
--- /*Standard Sheet*/"}
+SELECT 
+date(CAST(year as varchar) || '-' || CAST(month as varchar) || '-01') AS adaptive_month,
+      'Total Pinterest' AS Level,
+      user_country as user_country,
+      sum(conversions_checkouts_771) AS amount
+FROM Finops.conversion_metrics
+WHERE year >= 2019
+GROUP BY year, month, user_country
