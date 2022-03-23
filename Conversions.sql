2067103beed2c01e18ca7386e041a1255781dd2e
victorhongpinterest <92404950+victorhongpinterest@users.noreply.github.com>
2022-03-23 14:23:31 -0700

    Update Conversions.sql

--- Conversions.sql
+++ Conversions.sql
@@ -1,19 +1,8 @@
--- For JSON Conversion
--- {"sql":"
 SELECT
   date(
-    concat(
-      CAST(year AS varchar),
-      '-',
-      CAST(month AS varchar),
-      '-01'
-    )
-  ) AS adaptive_month,
+    concat(CAST(year AS varchar)||'-'||CAST(month AS varchar)||'-01') AS adaptive_month,
   'Total Pinterest' AS Level,
-  CASE
-    WHEN user_country = 'US' THEN 'c_US_Conversions'
-    ELSE 'c_INTL_Conversions'
-  END AS Account,
+  user_country as user_country,
   sum(conversions_checkouts_771) AS amount
 FROM
   Finops.conversion_metrics
@@ -22,9 +11,4 @@ WHERE
 GROUP BY
   year,
   month,
-  CASE
-    WHEN user_country = 'US' THEN 'c_US_Conversions'
-    ELSE 'c_INTL_Conversions'
-  END
--- JSON Conversion
--- /*Standard Sheet*/"}
+  user_country
